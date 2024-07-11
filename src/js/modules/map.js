05740f104f4bd34cf0e3d14648c2469faded276d
ArtHard <1artem1nazarov1@gmail.com>
2024-07-11 13:15:12 +0300

    finish

--- src/js/modules/map.js
+++ src/js/modules/map.js
@@ -1,44 +1,83 @@
 export async function initMap() {
 
-	const { YMap, YMapDefaultSchemeLayer, YMapDefaultFeaturesLayer, YMapMarker, YMapControls } = window.ymaps3;
-	const { YMapGeolocationControl, YMapZoomControl } = await ymaps3.import('@yandex/ymaps3-controls@0.0.1');
+	const mapDiv = document.getElementById('map'); //* Оболочка яндекс карты
+
+	//* Подключение пакетов и зависимостей
+	const { YMap, YMapDefaultSchemeLayer, YMapDefaultFeaturesLayer, YMapMarker, YMapControls, YMapControlButton } = window.ymaps3;
+	const { YMapGeolocationControl, YMapZoomControl } = await window.ymaps3.import('@yandex/ymaps3-controls@0.0.1');
 	const { YMapDefaultMarker } = await window.ymaps3.import('@yandex/ymaps3-markers@0.0.1');
 
-	const controls = new YMapControls({ position: 'bottom' });
-	const controls2 = new YMapControls({ position: 'top' })
-	controls2.addChild(new YMapGeolocationControl());
-	controls.addChild(new YMapZoomControl());
+	//* Controls
+	const zoomControl = new YMapControls({ position: 'left' });
+	const geolocationControl = new YMapControls({ position: 'top left', orientation: 'vertical' });
+	const fullscreenControl = new YMapControls({ position: 'top right' });
 
-	// DOM-элемент должен быть создан заранее, но его содержимое можно задать в любой момент.
-	const content = document.createElement('section');
-	const marker = new YMapMarker({
-		coordinates: [30.338928000000006, 59.943543564154155]
-	}, content);
-
-	let defaultMarker = new YMapDefaultMarker({
-		coordinates: [30.338928000000006, 59.943543564154155],
-		color: 'green'
-	});
-
-	// Иницилиазируем карту
+	//* Иницилиазируем карту
 	const map = new YMap(
-		// Передаём ссылку на HTMLElement контейнера
-		document.getElementById('map'),
+		//* Передаём ссылку на HTMLElement контейнера
+		mapDiv,
 
-		// Передаём параметры инициализации карты
+		//* Передаём параметры инициализации карты
 		{
 			location: {
-				// Координаты центра карты
 				center: [30.338928000000006, 59.943543564154155],
-				// Уровень масштабирования
 				zoom: 16
-			}
+			},
+			//* Поведение карты
+			behaviors: ['drag', 'pinchZoom', 'dblClick']
 		}
 	)
-		.addChild(new YMapDefaultSchemeLayer())
-		.addChild(new YMapDefaultFeaturesLayer())
-		.addChild(marker)
-		.addChild(controls)
-		.addChild(controls2);
-	content.innerHTML = '<h1 style="background:black;"><a href="#">Этот заголовок можно переносить</a></h1>';
+		.addChild(new YMapDefaultSchemeLayer({ theme: 'dark' })) //* Базовое наложение (обязательно)
+		.addChild(new YMapDefaultFeaturesLayer()) //* Наложение для меток
+		.addChild(zoomControl)
+		.addChild(geolocationControl)
+		.addChild(fullscreenControl);
+
+	//* Добавляем базовые контролы
+	geolocationControl.addChild(new YMapGeolocationControl());
+	zoomControl.addChild(new YMapZoomControl());
+
+	// DOM-элемент должен быть создан заранее, но его содержимое можно задать в любой момент.
+	// const content = document.createElement('section');
+	// const marker = new YMapMarker({
+	// 	coordinates: [30.338928000000006, 59.943543564154155]
+	// }, content);
+	// content.innerHTML = '<h1 style="background:black;"><a href="#">Этот заголовок можно переносить</a></h1>';
+
+	const defaultMarker = new YMapDefaultMarker({
+		coordinates: [30.338928000000006, 59.943543564154155],
+		// color: '#d4c17f', //* цвет базовой метки (можно менять в css)
+		// title: 'LoftHouse',
+		// subtitle: 'Жилой комплекс'
+	});
+
+	//* Добавляем маркер на карту
+	map.addChild(defaultMarker);
+
+	//* Добавляем иконку в кнопке как простой div элемент
+	const fullScreenElement = document.createElement('div');
+	fullScreenElement.className = 'fullscreen';
+
+	//* Событие fullscreenchange вызывается сразу после переключения браузера в полноэкранный режим или выхода из него.
+	document.addEventListener('fullscreenchange', function () {
+		fullScreenElement.classList.toggle('exit-fullscreen');
+	});
+
+	function fullScreenBtnHandler() {
+		//* document.fullscreenElement возвращает элемент, который в данный момент представлен в полноэкранном режиме в этом документе, или значение null, если полноэкранный режим в настоящее время не используется.
+		if (document.fullscreenElement) {
+			document.exitFullscreen();
+		} else {
+			//* Метод element.requestFullscreen() выдает асинхронный запрос на отображение элемента в полноэкранном режиме.
+			mapDiv.requestFullscreen();
+		}
+	}
+
+	//* Add YMapControlButton that will enable or disable fullscreen mode
+	const fullScreenBtn = new YMapControlButton({
+		element: fullScreenElement,
+		onClick: fullScreenBtnHandler
+	});
+
+	fullscreenControl.addChild(fullScreenBtn);
 }
